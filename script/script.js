a336757207e446077530aeb2996b261bc031ec3d
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2022-11-26 17:15:40 -0300

    Correções no formulario de compra

--- script/script.js
+++ script/script.js
@@ -1,3 +1,30 @@
 document.getElementById("btnFinalizar").addEventListener("click", () => {
-  window.open("./login.html");
+  console.log("Hello world");
+  const nCartao = document.getElementById("nCartao").value;
+  const CVV = document.getElementById("CVV").value;
+  let dataVenc = document.getElementById("dataVenc").value;
+  dataVenc = dataVenc.split("-").reverse().join("/");
+  const parcelas = document.getElementById("parcelas").value;
+  const nome = document.getElementById("nome").value;
+  let dataNasc = document.getElementById("dataNasc").value;
+  dataNasc = dataNasc.split("-").reverse().join("/");
+  const senha = document.getElementById("senha").value;
+  const email = document.getElementById("email").value;
+  const user = document.getElementById("user").value;
+
+  if (
+    (nCartao,
+    CVV,
+    dataNasc,
+    dataVenc,
+    parcelas,
+    nome,
+    senha,
+    email,
+    user == "" || senha.length < 8)
+  ) {
+    return;
+  } else {
+    window.open("./login.html");
+  }
 });
